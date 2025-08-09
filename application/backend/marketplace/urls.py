4695955832db8cb62b7140795433381b4adc7577
Stella Parker <116223316+whoIsStella@users.noreply.github.com>
2025-08-08 17:16:39 -0700

    Update urls.py

--- application/backend/marketplace/urls.py
+++ application/backend/marketplace/urls.py
@@ -3,36 +3,25 @@ from rest_framework.routers import DefaultRouter
 from .views import (MeView, LoginView, RegisterView, UserViewSet, ParkingSpaceViewSet,
                     BookingViewSet, PaymentMethodViewSet, PaymentViewSet, MessageViewSet, ReviewViewSet,
                     AdminActionViewSet, SupportTicketViewSet, ServiceRequestViewSet, NotificationViewSet)
-                    #create_review)
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-#from .serializers import PaymentMethodSerializer
-#from .models import PaymentMethod
- #Note: Litterally everything is tied to 'users' so 
- #there is no specific person assigned this to this endpoint.
-#If you see a broken componant associated with 'users' then just fix it.
-# You will learn how to use websockets to be able to
-#work on messages, notifications, and service requests.
+
 router = DefaultRouter()
-router.register(r'users', UserViewSet) #everyone  DONE-ISH
-router.register(r'spaces', ParkingSpaceViewSet) #juan  DONE
-router.register(r'bookings', BookingViewSet) # juan DONE
-router.register(r'payment-methods', PaymentMethodViewSet) # nathan  DONE
-router.register(r'payments', PaymentViewSet)  # nathan
-router.register(r'messages', MessageViewSet)  # stella DONE-ISH
-# the messages are late and out of order but, hey, at least they are there.
-router.register(r'reviews', ReviewViewSet) # el juliana
-router.register(r'admin-actions', AdminActionViewSet) # fatma
-router.register(r'support-tickets', SupportTicketViewSet) # krish   DONE
-router.register(r'service-requests', ServiceRequestViewSet) # krish  DONE
-router.register(r'notifications', NotificationViewSet)  # stella DONE
+router.register(r'users', UserViewSet) 
+router.register(r'spaces', ParkingSpaceViewSet) 
+router.register(r'bookings', BookingViewSet)
+router.register(r'payment-methods', PaymentMethodViewSet)
+router.register(r'payments', PaymentViewSet) 
+router.register(r'messages', MessageViewSet) 
+router.register(r'reviews', ReviewViewSet) 
+router.register(r'admin-actions', AdminActionViewSet) 
+router.register(r'support-tickets', SupportTicketViewSet) 
+router.register(r'service-requests', ServiceRequestViewSet) 
+router.register(r'notifications', NotificationViewSet)  
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', MeView.as_view(), name='me'),
-    #path('api/', include('marketplace.urls')) This is already being called in parkingbnb/urls.py and settings.py
 ]
 
 
